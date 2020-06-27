95a965e8f5dcb1760d9c8ef67794d9d25df0c95f
Jason Hoffman <hello@publikwerks.com>
2020-06-27 12:23:42 -0700

    reversed order of display to start with 300º and end with 0º

--- Ch1/temp_variation.c
+++ Ch1/temp_variation.c
@@ -7,6 +7,6 @@ main()
 
 	printf("\nºF\tºC\n");
 	printf("__________\n");
-	for (fahr = 0; fahr <= 300; fahr += 20)
+	for (fahr = 300; fahr >= 0; fahr -= 20)
 		printf("%3d %6.1f\n", fahr, (5.0/9.0)*(fahr-32));
 }
