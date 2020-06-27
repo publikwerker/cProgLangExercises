7e214f1db262d55303cc34448786a4ced21d4924
Jason Hoffman <hello@publikwerks.com>
2020-06-27 12:13:11 -0700

    added a C->F table as well.

--- Ch1/temp_table.c
+++ Ch1/temp_table.c
@@ -18,6 +18,16 @@ main()
 	{
 		celsius = (5.0/9.0) * (fahr-32.0);
 		printf("%3.0f %6.1f\n", fahr, celsius);
-		fahr+=step;
+		fahr += step;
+	}
+
+	printf("\n\n\nºC\tºF\n");
+	printf("__________\n");
+	celsius = lower;
+	while (celsius <= upper)
+	{
+		fahr = (9.0/5.0) * celsius + 32;
+		printf("%3.0f %6.1f\n", celsius, fahr);
+		celsius += step;
 	}
 }
